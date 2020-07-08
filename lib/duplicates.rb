dc5b4b5b5ba80b03bc52abe512a3561a740a433b
Jamie <jamiewong63@googlemail.com>
2020-07-08 21:36:22 +0100

    test input: bb output 2

--- lib/duplicates.rb
+++ lib/duplicates.rb
@@ -1,6 +1,11 @@
 class Duplicates
   def count(text)
-    return 1 if text == 'aa'
-    0
+    duplicate = {}
+    duplicates_array = []
+    text.split('').each do |char|
+      duplicate.has_key?(char) ? duplicates_array << char : duplicate[char] = 1
+    end
+    return 0 if duplicates_array.empty?
+    duplicates_array.uniq.length
   end
 end
